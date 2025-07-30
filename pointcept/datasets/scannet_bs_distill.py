aa6c94e8800838018f2b6b6e5a05240d2a583c96
Chae-yeong Song <song9961@gmail.com>
2025-07-30 14:20:07 +0900

    Add bug fix

--- pointcept/datasets/scannet_bs_distill.py
+++ pointcept/datasets/scannet_bs_distill.py
@@ -38,19 +38,24 @@ class ScanNetBSDistillDataset(ScanNetDataset):
         else:
             # boundary_root가 config에 없으면 0으로 채움
             data_dict['boundary'] = np.zeros(data_dict['coord'].shape[0], dtype=np.int32)
+            logger.warning(f"Boundary root not found at {boundary_path}. Filling with zeros.")
 
         # 3. Teacher가 사용할 Opacity 특징 로드
         if self.features_root:
             try:
-                features_path = os.path.join(self.features_root, scene_name, "features.npy")
+                features_path = os.path.join(self.features_root, self.split, scene_name, "features.npy")
                 all_features_3dgs = np.load(features_path)
                 # features.npy에서 opacity 데이터 추출 (scale(3), opacity(1) 순서 가정)
                 data_dict['features'] = all_features_3dgs[:, 3:4].astype(np.float32)
             except FileNotFoundError:
                 data_dict['features'] = np.zeros((data_dict['coord'].shape[0], 1), dtype=np.float32)
+                logger = get_root_logger()
+                logger.warning(f"Features file not found at path, {self.features_root} Filling with zeros.")
         else:
             data_dict['features'] = np.zeros((data_dict['coord'].shape[0], 1), dtype=np.float32)
-        
+            logger = get_root_logger()
+            logger.warning(f"Features file not found at {self.features_root}. Filling with zeros.")
+
         return data_dict
 
 @DATASETS.register_module()
